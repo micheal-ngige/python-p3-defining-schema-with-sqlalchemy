fb0432854ed842d24845568badea301f84844706
michael <michaelngige@gmail.com>
2023-12-12 12:49:44 +0300

    initial commit

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 
 from sqlalchemy import Column, Integer, String, create_engine
@@ -6,7 +7,4 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Student(Base):
-    pass
-
-if __name__ == '__main__':
     pass
